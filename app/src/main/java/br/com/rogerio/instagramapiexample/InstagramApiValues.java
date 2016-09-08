535f931e3833f4d7dc34c91fd2ddeebbfb756b98
Rogério Nóbrega <rogerio.nobrega@embedded.ufcg.edu.br>
2016-09-08 13:45:51 -0300

    removing api keys

---
+++ app/src/main/java/br/com/rogerio/instagramapiexample/InstagramApiValues.java
@@ -0,0 +1,17 @@
+/*******************************************************************
+ * Copyright (C) 2014 DL.                                           *
+ * All rights, including trade secret rights, reserved.             *
+ *******************************************************************/
+
+package br.com.rogerio.instagramapiexample;
+
+/**
+ * Created by rogerio on 08/09/16.
+ */
+public class InstagramApiValues {
+    public static final String CLIENT_ID = "<instagram client ID>";
+    public static final String EMAIL = "<instagram client email>";
+    public static final String SECRET = "<instagram client secret>";
+    public static final String AUTH_URL = "https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code";
+    public static final String REDIRECT_URI = "<instagram client redirect URI>";
+}
--- app/src/main/java/br/com/rogerio/instagramapiexample/InstagramService.java
+++ app/src/main/java/br/com/rogerio/instagramapiexample/InstagramService.java
@@ -9,18 +9,10 @@ package br.com.rogerio.instagramapiexample;
  * Created by rogerio on 26/08/16.
  */
 public class InstagramService {
-    public static final String CLIENT_ID = "b59755503caf4d3cb375631b316dec10";
-    public static final String EMAIL = "rogeriocnobrega@gmail.com";
-    public static final String SECRET = "e21a906c37f841098810cb8ea6dea6d0";
-
-    private static final String AUTH_URL = "https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code";
-
-    private static final String REDIRECT_URI = "http://www.insapiexample.com";
-
 
     public static String getAuthUrl() {
-        String authUrl = AUTH_URL.replace("CLIENT-ID", CLIENT_ID);
-        authUrl = authUrl.replace("REDIRECT-URI", REDIRECT_URI);
+        String authUrl = InstagramApiValues.AUTH_URL.replace("CLIENT-ID", InstagramApiValues.CLIENT_ID);
+        authUrl = authUrl.replace("REDIRECT-URI", InstagramApiValues.REDIRECT_URI);
 
         return authUrl;
     }
